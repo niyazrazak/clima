d57901ada393abbc33dc96152f25f584d837c30b
Niyaz Razak <niyasibnurazak@gmail.com>
2022-11-13 11:00:00 +0530

    clima

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,28 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  Future<void> getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/services/location.dart
@@ -0,0 +1,25 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  late double latitude;
+  late double longitude;
+  Future<void> getCurrentLocation() async {
+    try {
+      LocationPermission permission;
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.low);
+
+      latitude = position.latitude;
+      longitude = position.longitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
